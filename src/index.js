b43518ecc9f1ad1d098e16ba0382b0b453ebee2b
Maryna Butovych <marina.butovich@gmail.com>
2020-06-16 11:51:14 +0300

    routing added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
 import Planets from './Planets';
+import People from './People';
+import Starships from './Starships';
 import NavBar from './components/common/Navbar';
 
 const navItems = [
@@ -12,7 +16,14 @@ const navItems = [
 ReactDOM.render(
   <React.StrictMode>
     <NavBar name='JEDI' items={navItems}/>
-    <Planets />
+    <Router>
+       <Switch>
+         <Redirect exact from="/" to="/people"/>
+         <Route exact path="/people" component={People} />
+         <Route exact path="/planets" component={Planets} />
+         <Route exact path="/starships" component={Starships} /> */}
+       </Switch>
+     </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
